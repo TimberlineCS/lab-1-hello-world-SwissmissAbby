4a956f17d0b48722379eb1971418d52c6f16f876
SwissmissAbby <swissmissabby@gmail.com>
2020-08-21 15:06:23 +0000

    I completed Lab 1

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+//Abby Stamper
+//Computer Science 2nd Period
+// 8/20/20
+
+/*
+This code makes love ascii art
+*/
+public class HelloWorld{
+
+  public static void main (String args[]){
+  
+    System.out.println("▀██▀─▄███▄─▀██─██▀██▀▀█");
+    System.out.println("─██─███─███─██─██─██▄█");
+    System.out.println("─██─▀██▄██▀─▀█▄█▀─██▀█");
+    System.out.println("▄██▄▄█▀▀▀─────▀──▄██▄▄█");
+  }
+
+}
